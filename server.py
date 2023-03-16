60a2fe2700d23b1b67f68141694afd2935c588fc
vlinGit <vlin0812@gmail.com>
2023-03-15 21:27:37 -0400

    super basic chat app with web sockets

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+from starlette.responses import FileResponse #fastAPI is based off starlette
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def start():
+    return FileResponse("index.html")
 
-@app.get("/redirect")
-async def redirect():
-    return {"message": "Redirected"}
+@app.get("/websocket.js")
+async def script():
+    return FileResponse("websocket.js")
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
