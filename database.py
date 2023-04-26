1011f25d3d8b5b948c7007e41109a1d677f27b6f
mthoste <mthoste>
2023-04-26 18:01:29 -0400

    Authorization is done

--- database.py
+++ database.py
@@ -1,14 +1,46 @@
 from pymongo import MongoClient
+import html
+import bcrypt
+import secrets
+import json
 
 class db():
-    mongoClient = MongoClient("mongo")
-    database = mongoClient["database"]
-    userCollection = database["users"]
+
+    mongo_Client = MongoClient(
+        "mongodb+srv://mike:NqCsSQC8E8Arjcwd@cluster0.pkpo8iw.mongodb.net/?retryWrites=true&w=majority")
+    # mongo_Client = MongoClient("mongo")
+    db = mongo_Client["database"]
+    userCollection = db["users"]
+
+    # mongoClient = MongoClient("mongo")
+    # database = mongoClient["database"]
+    # userCollection = database["users"]
     
     # IMPORTANT: ENCRYPT THE PASSWORD, 
     # THIS IS CURRENTLY ONLY A PROOF OF CONCEPT FOR STORING USERS
     def storeUser(self, username, password):
-        self.userCollection.insert_one({"username": username, "password": password})
+
+        # Make sure that the user is logging in correctly
+        for user in self.userCollection.find({}):
+            if user == username:
+                return False
+
+        # Check for html injection
+        username = html.escape(username)
+        password = html.escape(password)
+        password = password.encode('utf-8')
+
+        # Salt the password || encrypt
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password, salt)
+
+        token = secrets.token_urlsafe(20)
+
+        self.userCollection.insert_one(
+            {"username": username, "password": hashed_password.decode(), "salt": salt.decode(), "score": 0, "auth": token})
+
+        return True
+
         
     def getUsers(self):
         users = []
@@ -18,3 +50,16 @@ class db():
             users.append(user)
             
         return users
+
+    def auth_User(self, username, password):
+
+        for user in self.userCollection.find({}):
+            print(user)
+            hashed_password = bcrypt.hashpw(password.encode(
+                "utf-8"), user.get('salt').encode("utf-8"))
+            if user.get("username") == username:
+                if user.get("password") == hashed_password.decode():
+                    print("User is now logged in and passing back auth token ")
+                    return user["auth"]
+
+        return ""
--- server.py
+++ server.py
@@ -17,9 +17,10 @@ async def getUsers():
 async def start():
     return FileResponse("src/html/index.html")
 
-@app.get("/signup.html")
+
+@app.get("/signup")
 async def start():
-    return FileResponse("src/html/signup.html")
+    return FileResponse("src/html/index.html")
 
 @app.get("/styles/main.css")
 async def start():
@@ -37,11 +38,31 @@ async def getLogo():
 async def script():
     return FileResponse("src/js/main.js")
 
-# Handles user login/signup
-@app.post("/data")
-async def storeUser(username: str = Form (...), password: str = Form (...)):
-    userdb.storeUser(username, password)
+# Handles user login
+
+
+@app.post("/login")
+async def getuser(username: str = Form(...), password: str = Form(...)):
+
+    return_Info = userdb.auth_User(username, password)
+
+    if return_Info == "":
+        # bad request here
+        print("bad")
+        # otherwise it sets the return_Info as a cookie of authorication which Will be checked
     return RedirectResponse("", status.HTTP_204_NO_CONTENT)
+
+# Handles Signup
+
+
+@app.post("/signup")
+async def storeUser(username: str = Form(...), password: str = Form(...)):
+    return_Type = userdb.storeUser(username, password)
+
+    # This is to make sure that a new user does not have the same name
+    if return_Type == True:
+        return RedirectResponse("", status.HTTP_204_NO_CONTENT)
+    return RedirectResponse("", status.HTTP_401_UNAUTHORIZED)
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -51,3 +72,11 @@ async def websocket_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8000)
+
+
+def auth_check(DataBase_Users, Incoming_Auth_Token):
+
+    for user in DataBase_Users:
+        if user["auth"] == Incoming_Auth_Token:
+            return True
+    return False
