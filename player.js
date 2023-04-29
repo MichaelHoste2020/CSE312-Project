666e90f3bbce57783a581f6afadadc414283d923
Ian-hunter1 <ianhunter@students.tburg.k12.ny.us>
2023-04-28 20:34:49 -0400

    added primitive collsion system and other game functionality

--- player.js
+++ player.js
@@ -1,11 +1,12 @@
 class Player extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, width, height, speed, color) { 
+    constructor(scene, x, y, width, height, speed, color, direction) { 
         // Create a texture with the specified color
         const graphics = new Phaser.GameObjects.Graphics(scene);    
         graphics.fillStyle(color, 1);
         graphics.fillRect(0, 0, width, height); 
         const texture = graphics.generateTexture('player', width, height);
         graphics.destroy(); 
+        
 
         // Fill in superclass constructors using the newly generated texture
         super(scene, x, y, 'player');
@@ -17,14 +18,15 @@ class Player extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);
 
         // Set the member variables
+        this.direction = "down"
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.speed = speed;
         this.color = color;
-        this.claimed_tiles = [];
-        this.path_tiles = [];
+        this.claimed_tiles = [x,y];
+        this.path_tiles = [x,y];
         this.controls = {
             up: scene.input.keyboard.addKey('W'),
             left: scene.input.keyboard.addKey('A'),
@@ -37,17 +39,29 @@ class Player extends Phaser.GameObjects.Sprite {
     update() {
         // Control the player
         if (this.controls.up?.isDown) {
-            this.y -= this.speed;
+            this.direction = "up"
         }
         if (this.controls.left?.isDown) {
-            this.x -= this.speed;
+            this.direction = "left"
         }
         if (this.controls.down?.isDown) {
-            this.y += this.speed;
+            this.direction = "down"
         }
         if (this.controls.right?.isDown) {
+            this.direction = "right"
+        }
+
+        if (this.direction == "up"){
+            this.y -= this.speed;
+        }else if (this.direction == "left"){
+            this.x -= this.speed;
+        }else if (this.direction == "down"){
+            this.y += this.speed;
+        }else if (this.direction == "right"){
             this.x += this.speed;
         }
+
+        this.path_tiles.push([this.x,this.y])
     }
 
 }
