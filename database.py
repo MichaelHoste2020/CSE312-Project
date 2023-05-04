602d5eedddb5e21582ee2c84a447fe7fe333f7ef
mthoste <mthoste>
2023-05-03 22:33:29 -0400

    Finished login and signout along with finishing the landing page which should be done along with user account information being able to be changed properly

--- database.py
+++ database.py
@@ -6,9 +6,9 @@ import json
 
 class db():
 
-    # mongo_Client = MongoClient(
-    #     "mongodb+srv://mike:NqCsSQC8E8Arjcwd@cluster0.pkpo8iw.mongodb.net/?retryWrites=true&w=majority")
-    mongo_Client = MongoClient("mongo")
+    mongo_Client = MongoClient(
+        "mongodb+srv://mike:NqCsSQC8E8Arjcwd@cluster0.pkpo8iw.mongodb.net/?retryWrites=true&w=majority")
+    # mongo_Client = MongoClient("mongo")
     db = mongo_Client["database"]
     userCollection = db["users"]
 
@@ -33,9 +33,11 @@ class db():
         hashed_password = bcrypt.hashpw(password, salt)
 
         token = secrets.token_urlsafe(20)
+        print("This is the type of the token {0}".format(type(token)))
+        token_Safe = bcrypt.hashpw(token.encode("utf-8"), salt)
 
         self.userCollection.insert_one(
-            {"username": username, "password": hashed_password.decode(), "salt": salt.decode(), "score": 0, "auth": token})
+            {"username": username, "password": hashed_password, "salt": salt, "score": 0, "auth": token_Safe})
 
         return True
 
@@ -53,11 +55,64 @@ class db():
 
         for user in self.userCollection.find({}):
             print(user)
-            hashed_password = bcrypt.hashpw(password.encode(
-                "utf-8"), user.get('salt').encode("utf-8"))
+            hashed_password = bcrypt.hashpw(
+                password.encode("utf-8"), user.get('salt'))
             if user.get("username") == username:
-                if user.get("password") == hashed_password.decode():
+                if user.get("password") == hashed_password:
                     print("User is now logged in and passing back auth token ")
-                    return user["auth"]
+
+                    token = secrets.token_urlsafe(20)
+                    storage = bcrypt.hashpw(
+                        token.encode("utf-8"), user.get("salt"))
+                    self.userCollection.update_one({"username": username}, {
+                        '$set': {"auth": storage}})
+                    return token
 
         return ""
+
+    def Auth_Cookie_Check(self, token):
+
+        for user in self.userCollection.find({}):
+            hashed_auth = bcrypt.hashpw(
+                token.encode("utf-8"), user.get("salt"))
+            if user.get("auth") == hashed_auth:
+                return True
+        return False
+
+    def Current_User(self, token):
+
+        for user in self.userCollection.find({}):
+            hashed_auth = bcrypt.hashpw(
+                token.encode("utf-8"), user.get("salt"))
+            if user.get("auth") == hashed_auth:
+                return user.get("username")
+        return ""
+
+    def Change_User_Info(self, username, password, token):
+
+        # no duplicate users
+        for user in self.userCollection.find({}):
+            if user == username:
+                return False
+
+        # Check for html injection
+        username = html.escape(username)
+        password = html.escape(password)
+
+        for user in self.userCollection.find({}):
+            hashed_auth = bcrypt.hashpw(
+                token.encode("utf-8"), user.get("salt"))
+            if user.get("auth") == hashed_auth:
+
+                if password != "":
+                    password = password.encode('utf-8')
+                    hashed_password = bcrypt.hashpw(password, user["salt"])
+                    self.userCollection.update_one({"username": user["username"]}, {
+                        '$set': {"password": hashed_password}})
+
+                if username != "":
+                    self.userCollection.update_one({"username": user["username"]}, {
+                        '$set': {"username": username}})
+
+                return True
+        return False
--- server.py
+++ server.py
@@ -3,51 +3,91 @@ import json
 from database import db
 from fastapi import *
 from fastapi.staticfiles import StaticFiles
-from starlette.responses import * #fastAPI is based off starlette
+from starlette.responses import *  # fastAPI is based off starlette
 from fastapi.templating import Jinja2Templates
 import bcrypt
+from functions import *
 
 app = FastAPI()
 userdb = db()
 
 # IMPORTANT: DELETE THIS, ONLY FOR DEMOING
+
+templates = Jinja2Templates(directory="src/html")
+
+
 @app.get("/users")
 async def getUsers():
     return userdb.getUsers()
 
+
+@app.post("/logout")
+async def getUsers(request: Request, response: Response):
+    response.delete_cookie(key="auth")
+    return RedirectResponse("/", status.HTTP_301_MOVED_PERMANENTLY)
+
+
 @app.get("/")
-async def start(request: Request, response: Response):
+async def login_signup(request: Request, response: Response):
     # grabbing the value of "visits" cookie from response headers
     visitsCookie = request.cookies.get("visits")
     # response starts off as FileResponse and then add a cookie on top of that with set_cookie
     response = FileResponse("src/html/index.html")
-    
+
     if visitsCookie:
         response.set_cookie(key="visits", value=int(visitsCookie)+1)
     else:
         response.set_cookie(key="visits", value=1)
     return response
 
+
 @app.get("/home")
+async def homePage(request: Request):
+
+    auth = request.cookies.get("auth_token")
+    print("This is the current auth token {0}".format(auth))
+
+    # If we are not authenticated return to main page
+    if auth == None:
+        return RedirectResponse("/", status.HTTP_301_MOVED_PERMANENTLY)
+    elif (userdb.Auth_Cookie_Check(auth) == False):
+        return RedirectResponse("/", status.HTTP_301_MOVED_PERMANENTLY)
+
+    name = userdb.Current_User(auth)
+
+    users = []
+
+    for user in userdb.userCollection.find({}):
+        users.append({"username": user["username"], "score": user["score"]})
+
+    return templates.TemplateResponse("home.html", {"request": request, "username": name, "users": users})
+
+
+@app.get("/styles/home.css")
 async def start():
-    name = find_Current_User()
-    return Jinja2Templates.TemplateResponse("src/html/home.html",{"username": name})
+    print("getting new css")
+    return FileResponse("src/styles/home.css")
+
 
 @app.get("/signup")
 async def start():
     return FileResponse("src/html/index.html")
 
+
 @app.get("/styles/main.css")
 async def start():
     return FileResponse("src/styles/main.css")
+
 
 @app.get("/styles/signup.css")
 async def start():
     return FileResponse("src/styles/signup.css")
 
+
 @app.get("/images/logo.svg")
 async def getLogo():
     return FileResponse("src/images/logo.svg")
+
 
 @app.get("/js/main.js")
 async def script():
@@ -55,16 +95,20 @@ async def script():
 
 # Handles user login
 
+
 @app.post("/login")
 async def getuser(username: str = Form(...), password: str = Form(...)):
 
+    # Check and see if they are already logged in
     return_Info = userdb.auth_User(username, password)
 
+    # bad request here
     if return_Info == "":
-        # bad request here
         print("bad")
-        # otherwise it sets the return_Info as a cookie of authorication which Will be checked
-    return RedirectResponse("/home", status.HTTP_301_MOVED_PERMANENTLY)
+        return RedirectResponse("/", status.HTTP_301_MOVED_PERMANENTLY)
+    response = RedirectResponse("/home", status.HTTP_301_MOVED_PERMANENTLY)
+    response.set_cookie(key="auth_token", value=return_Info)
+    return response
 
 # Handles Signup
 
@@ -78,35 +122,26 @@ async def storeUser(username: str = Form(...), password: str = Form(...)):
         return RedirectResponse("/", status.HTTP_301_MOVED_PERMANENTLY)
     return RedirectResponse("", status.HTTP_401_UNAUTHORIZED)
 
+
+@app.post("/change-account")
+async def change_Account_Info(request: Request,username: str = Form(...), password: str = Form(...)):
+    
+    cookie = request.cookies.get("auth_token")
+    
+    return_Type = userdb.Change_User_Info(username, password,cookie)
+
+    # This is to make sure that a new user does not have the same name
+    if return_Type == True:
+        return RedirectResponse("/home", status.HTTP_301_MOVED_PERMANENTLY)
+    return RedirectResponse("/", status.HTTP_401_UNAUTHORIZED)
+
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive()
 
+
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8000)
-
-
-def auth_check(DataBase_Users, Incoming_Auth_Token):
-
-    for user in DataBase_Users:
-        if user["auth"] == Incoming_Auth_Token:
-            return True
-    return False
-
-def find_Current_User(userdb,auth_Token):
-    for user in userdb:
-        check = bcrypt.hashpw(auth_Token,user["salt"])
-        if user["auth_Token"] == check:
-            return user["username"]
-        
-    return ""
-
-def list_User(user_db):
-
-    user_List = []
-
-    for user in user_db.find({}):
-        user_List.append({"username":user["username"],"Score":user["score"]})
-    return user_List
